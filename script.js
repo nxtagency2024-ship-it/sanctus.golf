ecd22496e187aea33a08ea8144701dd780de9cca
nxtagencyscaled <nxtagency2024@gmail.com>
2026-07-30 20:27:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -515,3 +515,17 @@ if (hamburger && navMenu) {
     });
 
 }
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("nav ul");
+
+if (hamburger && navMenu) {
+    hamburger.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+
+    document.querySelectorAll("nav ul a").forEach(link => {
+        link.addEventListener("click", () => {
+            navMenu.classList.remove("active");
+        });
+    });
+}
