4cde559a37a6989c173be0bee1d8ee8d55d9880b
nxtagencyscaled <nxtagency2024@gmail.com>
2026-07-30 21:00:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -504,3 +504,12 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 30) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
