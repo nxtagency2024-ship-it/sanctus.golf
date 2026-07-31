86f8cbdc25a6a8a09a5942617fd02e1e2c06cc3d
nxtagencyscaled <nxtagency2024@gmail.com>
2026-07-30 20:21:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -505,3 +505,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("nav ul");
+
+if (hamburger && navMenu) {
+
+    hamburger.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+
+}
